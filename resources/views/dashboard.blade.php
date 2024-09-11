68595178bf6f71f0099237c370fad7b771d0344d
nono <nonokub.671@gmail.com>
2024-09-11 20:06:24 +0700

    add dashboard file

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-    <div class="py-12">
+      <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
@@ -13,17 +13,11 @@
                             Hello, {{ Auth::user()->name }} !
                         </h1>
                         @if(Auth::user()->profile_photo)
-                            <img src="{{ asset('storage/' . Auth::user()->profile_photo) }}" 
-                                alt="Profile Photo" 
-                                class="w-40 h-40 object-cover rounded-full mx-auto">
+                            <img src="{{ asset('storage/' . Auth::user()->profile_photo) }}" alt="Profile Photo" class="rounded-full w-96 h-96 mb-4">
                         @else
-                            <img src="{{ asset('path/to/default-profile-photo.jpg') }}" 
-                                alt="Default Profile Photo" 
-                                class="w-40 h-40 object-cover rounded-full mx-auto">
+                            <p class="text-base text-gray-600 dark:text-gray-400 font-bold">{{ __('No profile photo uploaded.') }}</p>
                         @endif
-
-
-                        <p class = "font-semibold text-lg text-gray-800 dark:text-gray-200 leading-tight mb-4" >{{ ("This is my mugshot UwU") }}</p>
+                        <p class = "font-semibold text-lg text-gray-800 dark:text-gray-200 leading-tight mb-4" >{{ __("This is for my database lab class :D") }}</p>
                         <p class="font-base text-lg">{{ __("You're logged in!") }}</p>
                     </div>
                 </div>
